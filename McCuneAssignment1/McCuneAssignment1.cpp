0386b7d8bcfb97e4884eb6e6cc31b8fa41d23dde
AJ <mccunea@mymail.vcu.edu>
2017-02-08 10:35:01 -0500

    Code entered

--- McCuneAssignment1/McCuneAssignment1.cpp
+++ McCuneAssignment1/McCuneAssignment1.cpp
@@ -2,11 +2,43 @@
 //
 
 #include "stdafx.h"
-
+#include <iostream>
+using namespace std;
 
 int main()
 {
+	double balance;
+	double transaction;
+	char transactionType;
 
+	cout << "Please enter account balance:" << endl;
+	cin >> balance;
+	
+	do {
+		cout << "Enter transaction type (W)ithdrawl, (D)eposit, (C)heck, (Q)uit" << endl;
+		cin >> transactionType;
+
+		if (transactionType = 'W' || 'w')
+		{
+			cout << "Enter amount to be withdrawn:" << endl;
+				cin >> transaction;
+				balance -= transaction;
+
+		}
+		else if (transactionType = 'D' || 'd')
+		{
+			cout << "Enter amount to be deposited:" << endl;
+			cin >> transaction;
+			balance += transaction;
+		}
+		else if (transactionType = 'C' || 'c')
+		{
+
+		}
+
+	} while (transactionType != 'Q' || 'q');
+
+		cout << balance;
 	
     return 0;
 }
