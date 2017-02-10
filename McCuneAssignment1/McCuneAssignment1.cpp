f9dfdeec691baa5276479f09a7599d79f33b82d4
AJ <mccunea@mymail.vcu.edu>
2017-02-09 19:14:23 -0500

    Formatting complete, error handling complete, overdraft addition complete. All that is left are comments and making red/ green text

--- McCuneAssignment1/McCuneAssignment1.cpp
+++ McCuneAssignment1/McCuneAssignment1.cpp
@@ -11,18 +11,24 @@ int main()
 	double withdrawlArray[50];
 	double depositArray[50];
 	double checkArray[50];
+	double overdraftArray[50];
 	double beginningBalance;
 	double runningBalance;
 	double transaction;
+	double overdraft;
 	double withdrawlTotal = 0;
 	double depositTotal = 0;
 	double checkTotal = 0;
+	double overdraftTotal = 0;
 	int wcount = 0;
 	int dcount = 0;
 	int ccount = 0;
+	int ocount = 0;
 	int i;
 	char transactionType;
+	char overdraftConfirm;
 	bool valid;
+	bool didOverdraft = false;
 	cout << setprecision(2) << fixed;
 	
 	
@@ -41,7 +47,7 @@ int main()
 			do {
 				cout << "Enter amount to be withdrawn:" << endl;
 				cin >> transaction;
-
+				
 				valid = cin.fail();
 				cin.clear();
 				cin.ignore(numeric_limits<streamsize>::max(), '\n');
@@ -50,9 +56,41 @@ int main()
 					cout << "Entry invalid. Only use numbers or decimals." << endl;
 				}
 			} while (valid == true);
+			if (transaction > runningBalance)
+			{
+				do {
+					overdraft = (transaction * .05);
+					cout << "WARNING " << transaction << " is more money than you have in your account. You have "
+						<< runningBalance << " remaining." << "\n\n" <<
+						"If you apply this withdrawl a 5% overdraft fee (" << overdraft << ") will be applied to your account. Would you like to continue? Y/N\n";
+					cin >> overdraftConfirm;
+					if (overdraftConfirm == 'Y' || overdraftConfirm == 'y')
+					{
+						withdrawlArray[wcount] = transaction;
+						++wcount;
+						runningBalance -= transaction;
+						overdraftArray[ocount] = overdraft;
+						++ocount;
+						runningBalance -= overdraft;
+						didOverdraft = true;
+						
+
+					}
+					else if (overdraftConfirm == 'N' || overdraftConfirm == 'n') {
+
+					}
+					else {
+						cout << "Please enter Y/N: " << endl;
+					}
+				} while (overdraftConfirm != 'Y' && overdraftConfirm != 'y' && 
+					overdraftConfirm != 'N' && overdraftConfirm != 'n');
+
+			}
+			else {
 				withdrawlArray[wcount] = transaction;
 				++wcount;
 				runningBalance -= transaction;
+				}
 
 		}
 		else if (transactionType == 'D' || transactionType == 'd')
@@ -89,9 +127,43 @@ int main()
 				}
 			} while (valid == true);
 
+			if (transaction > runningBalance)
+			{
+				do {
+					overdraft = (transaction * .05);
+					cout << "WARNING " << transaction << " is more money than you have in your account. You have "
+						<< runningBalance << " remaining." << "\n\n" <<
+						"If you apply this withdrawl a 5% overdraft fee (" << overdraft << ") will be applied to your account. Would you like to continue? Y/N\n";
+					cin >> overdraftConfirm;
+					if (overdraftConfirm == 'Y' || overdraftConfirm == 'y')
+					{
+
+						checkArray[ccount] = transaction;
+						++ccount;
+						runningBalance -= transaction;
+						overdraftArray[ocount] = overdraft;
+						++ocount;
+						runningBalance -= overdraft;
+						didOverdraft = true;
+
+
+					}
+					else if (overdraftConfirm == 'N' || overdraftConfirm == 'n') {
+
+					}
+					else {
+						cout << "Please enter Y/N: " << endl;
+					}
+				} while (overdraftConfirm != 'Y' && overdraftConfirm != 'y' &&
+					overdraftConfirm != 'N' && overdraftConfirm != 'n');
+
+			}
+			else {
+
 				checkArray[ccount] = transaction;
 				++ccount;
 				runningBalance -= transaction;
+			}
 		}
 		else if (transactionType == 'Q' || transactionType == 'q')
 		{
@@ -134,8 +206,23 @@ int main()
 		cout  << "\t" << setw(34) << checkArray[i] << endl;
 		checkTotal += checkArray[i];
 	}
+
 	cout  << "\t----------------------------------" << endl;
-	cout  << "\tTotal checks: "  << setw(20) << checkTotal << endl << "------------------------------------------" << endl;
+	cout << "\tTotal checks: " << setw(20) << checkTotal << endl;
+	cout << "------------------------------------------" << endl;
+	if (didOverdraft == true)
+	{
+		cout << "\tOverdraft fees:" << endl;
+		for (i = 0; i < ocount; i++)
+		{
+			cout << "\t" << setw(34) << overdraftArray[i] << endl;
+			overdraftTotal += overdraftArray[i];
+		}
+
+		cout << "\t----------------------------------" << endl;
+		cout << "\tTotal overdraft fees: " << setw(12) << overdraftTotal << endl;
+		cout << "------------------------------------------" << endl;
+	}
 	cout << "\tRemaining balance: " << setw(15) << runningBalance << endl << "\n\n\n\n\n";
     return 0;
 }
